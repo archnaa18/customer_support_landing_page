09c2a019bdea8860ebd7c6bb89effef737cb6e11
Archna Chaturvedi <121455942+archnaa18@users.noreply.github.com>
2023-07-15 13:05:53 +0530

    Add files via upload

---
+++ customer_care_service_landing_page/javascript/app.js
@@ -0,0 +1,77 @@
+//---------------Read More and Read Less----------
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction1() {
+  var dots1 = document.getElementById("dots1");
+  var moreText1 = document.getElementById("more1");
+  var btnText1 = document.getElementById("myBtn1");
+
+  if (dots1.style.display === "none") {
+    dots1.style.display = "inline";
+    btnText1.innerHTML = "Read more";
+    moreText1.style.display = "none";
+  } else {
+    dots1.style.display = "none";
+    btnText1.innerHTML = "Read less";
+    moreText1.style.display = "inline";
+  }
+}
+
+function myFunction2() {
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("myBtn2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    btnText2.innerHTML = "Read more";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none";
+    btnText2.innerHTML = "Read less";
+    moreText2.style.display = "inline";
+  }
+}
+
+$(document).ready(function(){
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    center:true,
+    nav:true,
+    dots:false,
+    navText:["<i class='bx bx-skip-previous-circle' ></i>","<i class='bx bx-skip-next-circle'></i>"],
+    responsive:{
+        0:{
+            items:1,
+            
+        },
+        600:{
+            items:3,
+            
+        },
+        1000:{
+            items:3,
+            
+        }
+    }
+  })
+
+});
